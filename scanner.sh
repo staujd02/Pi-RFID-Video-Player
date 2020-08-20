66e08c9b4e8144b8e26f08083b94a3af615d492d
Joel Stauffer <altosax13@gmail.com>
2020-08-19 21:47:45 -0400

    revert: fix bad change pulled in

--- scanner.sh
+++ scanner.sh
@@ -7,7 +7,7 @@ declare -i c=1
 touch bash.temp
 
 # Search media directory for all .mp4 files (ignore all errors)
-find $1 -type f -name '*.avi' 2>/dev/null | while read entry;
+find $1 -type f -name '*.mp4' 2>/dev/null | while read entry;
 do
 #  For each entry, Create tuple: counter,file's name,file path (Space safe)
   echo "$((c++)),$(echo $entry | sed 's/^.*\///g'),${entry//\ /"\\ "}" >> bash.temp
