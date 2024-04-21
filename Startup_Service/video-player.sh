2d10dace9b942925c901dabb87a3a474ce056150
staujd02 <altsax13@gmail.com>
2024-04-21 13:34:06 -0400

    clean-up: user stuff

--- Startup_Service/video-player.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /home/athos/repos/bin/activate
-cd /home/athos/repos/Pi-RFID-Video-Player
-python3 videoEngine.pyw
---
+++ launch-editor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ~/repos/bin/activate
+cd ~/repos/Pi-RFID-Video-Player
+~/repos/bin/python3 main.pyw
